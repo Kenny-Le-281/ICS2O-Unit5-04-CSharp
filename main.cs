e1e0ddc65f7fca1222fe3299f31fc1196891f23c
Kenny Le <99355017+Kenny-Le-281@users.noreply.github.com>
2022-05-06 02:07:20 +0000

    changes in main.cs

--- main.cs
+++ main.cs
@@ -12,40 +12,34 @@ class Program
         int date;
         int age;
 
+        // input
         Console.WriteLine("What will the price of the museum admission be?");
         Console.WriteLine("");
 
-        void admissionday()
-        {
-            Console.WriteLine("Please enter the day");
-            Console.WriteLine("");
-            Console.WriteLine("1 - Sunday");
-            Console.WriteLine("2 - Monday");
-            Console.WriteLine("3 - Tuesday");
-            Console.WriteLine("4 - Wednesday");
-            Console.WriteLine("5 - Thursday");
-            Console.WriteLine("6 - Friday");
-            Console.WriteLine("7 - Saturday");
-            date = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Please enter the day");
+        Console.WriteLine("");
+        Console.WriteLine("1 - Sunday");
+        Console.WriteLine("2 - Monday");
+        Console.WriteLine("3 - Tuesday");
+        Console.WriteLine("4 - Wednesday");
+        Console.WriteLine("5 - Thursday");
+        Console.WriteLine("6 - Friday");
+        Console.WriteLine("7 - Saturday");
+        date = Convert.ToInt32(Console.ReadLine());
 
-            if (date > 7 || date < 1)
-            {
-                Console.Clear();
-                Console.WriteLine("Please input a valid date");
-                admissionday();
-            }
+        // process and output
+        if (date > 7 || date < 1)
+        {
+            Console.Clear();
+            Console.WriteLine("Please input a valid date");
         }
 
-        void admissionage()
-        {
-            Console.WriteLine("");
-            Console.WriteLine("Please enter your age:");
-            age = Convert.ToInt32(Console.ReadLine());
-        }
+        // input
+        Console.WriteLine("");
+        Console.WriteLine("Please enter your age:");
+        age = Convert.ToInt32(Console.ReadLine());
 
-        admissionday();
-        admissionage();
-
+        // process and output
         if ((date == 3 || date == 5) && (age > 12 && age < 21))
         {
             Console.WriteLine("You are eligible for student pricing!");
